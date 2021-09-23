7a41f69790390e6cb9982dd57f30e56c5b74988d
marineolvr <87575559+marineolvr@users.noreply.github.com>
2021-09-23 13:17:40 -0500

    Add files via upload

---
+++ Chykalophia_intagram.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+
+#imports for selenium WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+#google chrome webdriver
+driver = webdriver.Chrome()
+driver.get("https://chykalophia.com/")
+
+driver.maximize_window()
+
+#clicking on the button to go to instagram
+instagram_btn = driver.find_element_by_xpath('/html/body/div[2]/div/section[2]/div/div[2]/div/div[3]/div/div/span[4]/a')
+instagram_btn.click()
+
+#webdriverwait
+instagram_btn = driver.find_element_by_xpath('')
+instagram_btn.click()
+wd_wait = WebDriverWait(driver, 10)
+instagram_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "")))
+
+driver.quit()
+
+
